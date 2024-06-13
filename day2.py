883ca241fd9a562c9cc58b12a29c0bb359848bd6
rshepherdcpp <113625774+robertshepherdcpp@users.noreply.github.com>
2024-06-13 17:17:57 +0100

    add second day

---
+++ day2.py
@@ -0,0 +1,17 @@
+# Write a program that prints your name, age, and a favorite quote.
+
+print("Name: Robert")
+print("Age: 15")
+print("Favourite quote: ")
+
+
+num = 5
+if num >= 5:
+    print("num >= 5")
+else:
+
+    print("num < 5")
+
+"""
+This is for day 2 of the python project, and there are 3 exercises in this one.
+"""
