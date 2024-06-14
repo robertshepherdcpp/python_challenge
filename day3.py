fa01d61d835822aa12e61d1f16e2b5083fe18735
rshepherdcpp <113625774+robertshepherdcpp@users.noreply.github.com>
2024-06-14 07:12:44 +0100

    add day 3

---
+++ day3.py
@@ -0,0 +1,21 @@
+# name, age, favorite color, and whether you are a student or not.
+
+name = "Robert"
+age = 15
+favorite_color = "blue"
+is_student = True
+
+print("Name:", name)
+print("Age:", age)
+print("Favorite Colour:", favorite_color)
+print("Is Student:", is_student)
+
+print(type(name))
+print(type(age))
+print(type(favorite_color))
+print(type(is_student))
+
+age_next_year = age
+age_next_year += 1
+
+print("age next year:", age_next_year)
