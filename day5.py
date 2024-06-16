208a5db505e2bdb15c04a749f407e36adb661cef
rshepherdcpp <113625774+robertshepherdcpp@users.noreply.github.com>
2024-06-16 01:10:06 +0100

    add day 5

---
+++ day5.py
@@ -0,0 +1,14 @@
+# Write a program that checks if a number is positive and prints a message if it is.
+
+num = 5
+
+if num > 0:
+    print("num is positive")
+elif num < 0:
+    print("num is negative")
+else:
+    print("num is 0")
+
+# Write a program that checks if a number is even or odd and prints an appropriate message
+
+# Write a program that checks if a number is negative, zero, or positive and prints an appropriate message.
